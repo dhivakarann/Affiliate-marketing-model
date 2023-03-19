9e42da5b7fa278d65ac46600c7a2ed0da468565e
dhivakarann <126103575+dhivakarann@users.noreply.github.com>
2023-03-19 19:03:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+function myfunction(id){
+    var x = document.getElementById(id);
+    if(x.style.display =="block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
